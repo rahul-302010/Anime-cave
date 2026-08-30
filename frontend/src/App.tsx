fa372bf88dd441c795f5128c0c182aef563734fc
rahul-302010 <rahulchitra20101988@gmail.com>
2026-08-30 12:23:30 +0530

    feat(frontend): add App shell

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ExplorePage from './pages/ExplorePage'
+
+export default function App(){
+  return (
+    <div style={{minHeight:'100vh',background:'var(--background)'}}>
+      <ExplorePage />
+    </div>
+  )
+}
